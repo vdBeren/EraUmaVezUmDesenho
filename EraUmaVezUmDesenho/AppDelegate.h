16b1011be37b6a3a71f5797512979008f3815380
vdBeren <vdias.savariego@gmail.com>
2015-05-01 21:48:56 -0300

    Initial Commit

---
+++ EraUmaVezUmDesenho/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EraUmaVezUmDesenho
+//
+//  Created by Victor D. Savariego on 1/5/15.
+//  Copyright (c) 2015 Four Kids. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
