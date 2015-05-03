e5f47b7684d69578995ffee33decd6ec173d1396
vdBeren <vdias.savariego@gmail.com>
2015-05-02 21:42:35 -0300

    50%

---
+++ EraUmaVezUmDesenho/EVDPageViewController.h
@@ -0,0 +1,53 @@
+//
+//  EVDPageViewController.h
+//  EraUmaVezUmDesenho
+//
+//  Created by Victor D. Savariego on 1/5/15.
+//  Copyright (c) 2015 Four Kids. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "EVDPage.h"
+
+@interface EVDPageViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+{
+    
+    float r, g, b, alpha;
+    NSInteger brushWidth, selectedColor, selectedWidth;
+    CGPoint touchLastPoint, touchCurrentPoint, touchLocation;
+    UIImage *imageRecord, *imagePause, *imagePlay, *imageStop, *imageNarrate, *imageNarratePause;
+    NSURL *temporaryRecFileURL;
+    BOOL fetchAudio, fetchRecord;
+}
+
+@property (nonatomic) UIImageView *drawImage;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnRecordPause;
+@property (weak, nonatomic) IBOutlet UIButton *btnStop;
+@property (weak, nonatomic) IBOutlet UIButton *btnPlay;
+@property (weak, nonatomic) IBOutlet UIButton *btnColorBox;
+
+@property (weak, nonatomic) IBOutlet UIImageView *bgView;
+@property (weak, nonatomic) IBOutlet UIImageView *drawView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageLabelPlayPause;
+@property (weak, nonatomic) IBOutlet UIImageView *imageLabelStop;
+@property (weak, nonatomic) IBOutlet UIImageView *imageLabelRecord;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblPageText;
+@property (weak, nonatomic) IBOutlet UILabel *lblPageIndex;
+
+@property (nonatomic) NSMutableArray *btnColorFan;
+@property (nonatomic) NSMutableArray *btnWidthFan;
+
+@property (nonatomic) AVAudioRecorder *audioRecorder;
+@property (nonatomic) AVAudioPlayer *audioPlayer;
+
+@property (nonatomic) EVDPage *currentPage;
+@property (nonatomic) NSString* currentBookKey;
+
+-(void) setCurrentPage:(EVDPage *)currentPage;
+-(void) setCurrentBookKey:(NSString *)currentBookKey;
+-(void) setButtonsSettingsForCurrentUser;
+
+@end
---
+++ EraUmaVezUmDesenho/EVDSounds.m
@@ -0,0 +1,74 @@
+//
+//  EVDSounds.m
+//  EraUmaVezUmDesenho
+//
+//  Created by Victor D. Savariego on 2/5/15.
+//  Copyright (c) 2015 Four Kids. All rights reserved.
+//  http://www.freesfx.co.uk
+//
+
+#import "EVDSounds.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface EVDSounds ()
+
+{
+    AVAudioPlayer *click;
+    AVAudioPlayer *background;
+}
+
+@end
+
+@implementation EVDSounds
+
+- (void) playClique:(int)select{
+    
+    NSString *path, *name;
+    NSString *bundle = [[NSBundle mainBundle] resourcePath];
+    NSURL *soundUrl;
+    
+    switch (select) {
+        case 0:
+            name = @"clickLapisEspessura.mp3";
+            break;
+        case 1:
+            name = @"home.mp3";
+            break;
+        case 2:
+            name = @"home.mp3";
+            break;
+        case 3:
+            name = @"home.mp3";
+            break;
+        case 4:
+            name = @"pageProx.mp3";
+            break;
+        case 5:
+            name = @"finalizar.mp3";
+            break;
+        case 6:
+            name = @"caixaLapis.mp3";
+            break;
+        case 7:
+            name = @"clickLapisEspessura.mp3";
+            break;
+        case 8:
+            name = @"page.mp3";
+            break;
+    }
+    
+    path = [NSString stringWithFormat:@"%@/%@.mp3", bundle, name];
+    
+    soundUrl = [NSURL fileURLWithPath:path];
+    click = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+    click.numberOfLoops = 0;
+    [click setVolume:0.1];
+    
+    if (select == 4) {
+        [click setVolume:0.01];
+    }
+    
+    [click play];
+}
+
+@end
